e96fada029a251852470b78e16c3e017c8742c99
unknown <YaroslavS@YaroslavS.datasoft.local>
2017-03-13 15:04:19 +0200

    Null change for OrderDate

--- Tables/dbo.Orders.sql
+++ Tables/dbo.Orders.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Orders] (
   [OrderID] [int] NOT NULL,
   [CustomerID] [int] NOT NULL,
-  [OrderDate] [datetime] NULL,
+  [OrderDate] [datetime] NOT NULL,
   [ExpectedDeliveryDate] [datetime] NULL,
   [CustomerNotes] [varchar](500) NULL,
   CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED ([OrderID])
